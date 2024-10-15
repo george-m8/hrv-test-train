854992acf67f131a685231182fcfb815f06c50b4
georgem <geornb@gmail.com>
2024-10-15 11:18:26 +0100

    update pca

--- perform_pca.py
+++ perform_pca.py
@@ -1,9 +1,14 @@
 import sys
+import os
 import pandas as pd
 from sklearn.preprocessing import StandardScaler 
 from sklearn.decomposition import PCA
 
 debug = True
+
+# Function to list CSV files in a directory
+def list_csv_files(directory):
+    return [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith('.csv')]
 
 def load_csv_data(file_path):
     # Load the compiled MFCC data from CSV
@@ -39,11 +44,14 @@ def apply_pca(data, n_components):
 def main(data, n_components):
     # Load the data from CSV
     csv_file_path = data
-    data = load_csv_data(csv_file_path)
+    csv_data = load_csv_data(csv_file_path)
+
+    # Store the csv file name
+    csv_file_name = os.path.basename(csv_file_path)
     
     # Preprocess the data
     exclude_columns = ['HRV (ms)']  # Columns to exclude from PCA
-    data = preprocess_data(data, exclude_columns)
+    data = preprocess_data(csv_data, exclude_columns)
     
     # Standardize the data
     standardized_data = standardize_data(data)
@@ -60,13 +68,30 @@ def main(data, n_components):
     
     # Optionally save the PCA result to a new CSV
     pca_df = pd.DataFrame(pca_result)
-    pca_df.to_csv("pca_transformed_data.csv", index=False)
+
+    # Need to add back the 'HRV (ms)' column, append as first column
+    pca_df.insert(0, 'HRV (ms)', csv_data['HRV (ms)'])
+
+    # Create a new directory to store PCA features
+    os.makedirs("pca_features", exist_ok=True)
+
+    pca_df.to_csv(f"./pca_features/{csv_file_name}_pca_n_components={n_components}.csv", index=False)
     
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python perform_pca.py <data.csv>")
+        print("Usage: python perform_pca.py <csv directory>")
         sys.exit(1)
     else:
         # get file path from command line argument
-        file = sys.argv[1]
-        main(file, 10)  # Perform PCA with 10 components
+        directory = sys.argv[1]
+        files = list_csv_files(directory)
+
+        print(f"Found {len(files)} CSV files in the directory.")
+
+        n_components_array = [5, 10, 20, 50, 100]
+        
+        for file in files:
+            for n_components in n_components_array:
+                print(f"Processing file: {file}")
+                print(f"Number of components: {n_components}")
+                main(file, n_components)
